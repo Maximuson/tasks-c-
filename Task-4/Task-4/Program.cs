0277d1161d89c316f0a5e5641b73572651ed6214
Maximuson <maxpavlenko54@gmail.com>
2019-09-24 14:56:05 +0300

    complete task 4

--- Task-4/Task-4/Program.cs
+++ Task-4/Task-4/Program.cs
@@ -6,7 +6,36 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           int[] graph = new int[] { 1, 2, 3,2,4,5 };
+            int countIncrement = 0;
+            int countDecrement = 0;
+            bool isIncrementing = false;
+            bool isDecrementing = false;
+
+            for(int i =0; i<graph.Length-2; i++)
+            {
+                if(graph[i+1] > graph[i] && isIncrementing == false)
+                {
+                    isDecrementing = false;
+                    isIncrementing = true;
+                    countIncrement++;
+                }
+                else
+                if(graph[i+1] < graph[i] && isDecrementing == false)
+                {
+                    isDecrementing = true;
+                    isIncrementing = false;
+                    countDecrement++;
+
+                  
+                   
+                }
+            }
+
+            Console.WriteLine("Зроста " + countIncrement);
+            Console.WriteLine("Cпада " + countDecrement);
+            Console.ReadLine();
+            
         }
     }
 }
